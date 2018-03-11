591f457239cdd726291a4317427430198b2ec118
Simerjeet-Singh <35548123+Simerjeet-Singh@users.noreply.github.com>
2018-03-11 16:39:27 -0700

    Add files via upload

--- maxTweeter.c
+++ maxTweeter.c
@@ -1,1 +1,12 @@
-Now you are definitely reported
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main(){
+        int a = 1;
+        int b = 2;
+        
+        printf("after swap values are %d %d\n\n",a,b);
+        return 0;
+}
